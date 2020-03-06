0d100c2858d197f02b9777d4a10c2eb356463690
Alejandro Socorro <alejandrosocorro@gmail.com>
2020-03-06 02:30:04 +0100

    Renamed structs.

--- src/journey.rs
+++ src/journey.rs
@@ -1,49 +1,46 @@
-#![allow(missing_docs)] 
+#![allow(missing_docs)]
 
 use chrono::prelude::*;
-use failure::*;
 use structopt::*;
+use failure::*;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use std::io::Write;
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
+#[derive(StructOpt)]
+#[structopt(name = "jny", about = "journey is a command line tool to keep track of your journey and to help you get organized.")]
+enum Opts {
+    #[structopt(name = "new")]
+    New(New),
+}
+
+#[derive(StructOpt)]
+enum New {
+    #[structopt(name = "bullet")]
+    Bullet,
+}
+
 pub fn start() -> Result<(), Error> {
-    let opts = Opts::from_args();
+let opts = Opts::from_args();
 
     match opts {
         Opts::New(new) => match new {
-            New::Entry => new_entry()?,
+            New::Bullet => new_bullet()?,
         },
     }
 
     Ok(())
 }
 
-fn new_entry() -> Result<(), Error> {
+fn new_bullet() -> Result<(), Error> {
     let mut rl = Editor::<()>::new();
-    
-    println!("Date: (yyyy-mm-dd)");
-    let date_string = rl
-        .readline(">>> ")?
-        .split('-')
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
-    
-    let date = Utc.ymd(
-        date_string[0].parse::<i32>()?,
-        date_string[1].parse::<u32>()?,
-        date_string[2].parse::<u32>()?,
-    );
 
-    println!("\nTag (empty for no tag)");
-    let tag_name = rl.readline(">>> ")?;
-    
-    println!("\nEntry: (ctrl-d) to finish");
+    println!("\nPress (ctrl-d) to finish");
     let mut info = String::new();
 
     loop {
-        match rl.readline(">>> ") {
+        match rl.readline(">>>  ") {
             Ok(line) => {
                 info.push_str(&line);
                 info.push('\n');
@@ -55,38 +52,38 @@ fn new_entry() -> Result<(), Error> {
         }
     }
 
-    let entry = Entry {
-        date,
-        tag: Tag{name: tag_name},
-        info: info.trim().to_string(),
+    let bullet = Bullet {
+        date: Utc::now().date(),
+        tag: Tag::default(),
+        category: Category::Task,
+        value: info.trim().to_string(),
     };
 
-    entry.print()?;
+    bullet.print()?;
     Ok(())
 }
 
-#[derive(StructOpt)]
-#[structopt(name = "jny", about = "journey is a command line tool to keep track of your journey and to help you get organized.")]
-enum Opts {
-    #[structopt(name = "new")]
-    New(New),
+#[derive(Debug)]
+enum Category {
+    Task,
+    Note,
+    Event,
 }
 
-#[derive(StructOpt)]
-enum New {
-    #[structopt(name = "entry")]
-    Entry,
+#[derive(Debug, Default)]
+struct Tag {
+    name: String,
 }
 
 #[derive(Debug)]
-struct Entry {
+struct Bullet {
     tag: Tag,
     date: Date<Utc>,
-    info: String,
+    category: Category,
+    value: String,
 }
 
-impl Entry {
-
+impl Bullet {
     fn print(&self) -> Result<(), Error> {
         let mut stdout = StandardStream::stdout(ColorChoice::Always);
         stdout.set_color(ColorSpec::new().set_fg(Some(Color::White)))?;
@@ -95,14 +92,8 @@ impl Entry {
         writeln!(&mut stdout, "{}", self.date)?;
         stdout.reset()?;
         writeln!(&mut stdout)?;
-        writeln!(&mut stdout, "{}", self.info)?;
+        writeln!(&mut stdout, "{}", self.value)?;
         writeln!(&mut stdout)?;
         Ok(())
     }
-
-}
-
-#[derive(Debug)]
-struct Tag {
-    name: String,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-//! journey is a command line tool to keep track of your journey and to help you get organized. 
+//! journey is a command line tool to keep track of your journey and to help you get organized.
 
 pub use journey::start;
 
